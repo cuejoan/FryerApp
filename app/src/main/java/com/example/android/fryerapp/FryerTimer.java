9b75ebaaacda997fa2a0f92d4b0346a80e86ba9f
Joan Cue <cuejoan1@gmail.com>
2017-02-06 16:21:12 -0800

    - Added CustomCountDownTimer to be able to pause and resume the timer
    - Created an anonymous CountDownTimer object with its implementation.
    
     - Set onClickListener to zoneBText and done some implementation, but it's not completed

--- app/src/main/java/com/example/android/fryerapp/FryerTimer.java
+++ app/src/main/java/com/example/android/fryerapp/FryerTimer.java
@@ -19,6 +19,9 @@ public class FryerTimer extends AppCompatActivity {
     // this variable is to show that the timer has started
     private boolean timerHasStarted = false;
 
+    // this variable is to show that the timer B has started
+    private boolean timerBIsRunning = false;
+
     private CountDownTimer countDownTimer;
     //casting views to variables
     public RelativeLayout zoneARelativeLayout;
@@ -34,11 +37,15 @@ public class FryerTimer extends AppCompatActivity {
     //this shows "Zone A" before the timer is started
     public TextView zoneAText;
 
+    // This view displays Zone B
+    public TextView zoneBText;
+
+
     //this variable holds the start time for the timer
     private final long startTime = 30 * 1000;
 
-    //this bariable divides the miliseconds
-    private final long interval = 1000;
+    //this variable divides the milliseconds
+    private final long interval = 1 * 1000;
 
     //when when we puse the timer with a cancel(); then the timr has to save where it is paused, so that it knows wheree to restart
     //that is done at this variable
@@ -46,6 +53,10 @@ public class FryerTimer extends AppCompatActivity {
 
     //helps define actions in the "if" statement for when the timer is paused
     private boolean timerIsPaused = false;
+
+    //helps define actions in the "if" statement for when the timer is paused
+    private boolean timerBIsPaused = false;
+
     // this is defined in the onMonveListener to show where timertextA is on a move
     float dX, dY;
 
@@ -54,6 +65,19 @@ public class FryerTimer extends AppCompatActivity {
 
     //after a menu item is touched then we say that it is selected.  This variable will help us decided what to do when the menu item is selected
     public boolean menuItemIsSelected = false;
+
+
+    CustomCountDownTimer countDownTimerB = new CustomCountDownTimer(startTime, interval) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            zoneBText.setText("" + millisUntilFinished/1000);
+        }
+
+        @Override
+        public void onFinish() {
+            zoneBText.setText("Time's up!");
+        }
+    };
 
 
     @Override
@@ -91,7 +115,6 @@ public class FryerTimer extends AppCompatActivity {
                     //Handle selected state change
                 } else {
                     menuItemIsSelected = false;
-
                     menuItem.setBackgroundResource(R.drawable.menu_button);
                     Toast.makeText(getApplicationContext(), "The Menu Was DeSelected",
                             Toast.LENGTH_LONG).show();
@@ -110,16 +133,16 @@ public class FryerTimer extends AppCompatActivity {
 
             @Override
             public boolean onTouch(View view, MotionEvent event) {
-            //ToDo add code to start moving the timer after it has been moved a little.  If we don't do that then it gets confused wit
+                //ToDo add code to start moving the timer after it has been moved a little.  If we don't do that then it gets confused wit
                 // Todo I want the touch to pause the timer
                 // Todo add code so that when the animation is finished it will destroy timer we can use destroyCountdownTimer
                 // Todo add code so that after the animation is finished it will return ZoneATimerText to the original position
                 switch (event.getAction()) {
 
-//                    case MotionEvent.ACTION_DOWN:
-//                        dX = view.getX() - event.getRawX();
-//                        dY = view.getY() - event.getRawY();
-//                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        dX = view.getX() - event.getRawX();
+                        dY = view.getY() - event.getRawY();
+                        break;
 
                     case MotionEvent.ACTION_MOVE:
 
@@ -141,11 +164,11 @@ public class FryerTimer extends AppCompatActivity {
             }
         });
 
-        /* depending on the state of the timer, it needs to execute different code
-        For example, if the timer hasn't started and a menu item is selected then we should start the timer in that z zone when
-        we touch it.  If the timer has started and we touch then zone then the timer should pause
+       /* depending on the state of the timer, it needs to execute different code
+       For example, if the timer hasn't started and a menu item is selected then we should start the timer in that z zone when
+       we touch it.  If the timer has started and we touch then zone then the timer should pause
 
-         */
+        */
         zoneARelativeLayout = (RelativeLayout) findViewById(R.id.zoneA_relative_layout);
         zoneARelativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -155,7 +178,6 @@ public class FryerTimer extends AppCompatActivity {
                 //if the menu item is selected and the timer has not started then change background of the zoneARelativeLaoyout
                 // and start the timer
                 // ToDo I think this code can probably be cleaned up a lot and maybe add some break statements to make it run faster. I'm not 100% sure
-
                 if ((menuItemIsSelected) & (!timerHasStarted) & (timerIsPaused)) {
                     timerIsRunningViews();
                     countDownTimer.cancel();
@@ -165,8 +187,8 @@ public class FryerTimer extends AppCompatActivity {
                     timerHasStarted = true;
                     menuItem.setBackgroundResource(R.drawable.menu_button);
                     timerIsPaused = false;
-                    // if the menu item is selected and time has not started or paused then we should start the timer
 
+                // if the menu item is selected and time has not started or paused then we should start the timer
                 } else if ((menuItemIsSelected) & (!timerHasStarted) & (!timerIsPaused)) {
                     timerIsRunningViews();
                     countDownTimer.cancel();
@@ -216,7 +238,36 @@ public class FryerTimer extends AppCompatActivity {
         });
 
 
+        zoneBText = (TextView) findViewById(R.id.zoneB_text);
+        zoneBText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (menuItemIsSelected) {
+                    countDownTimerB.start();
+                    menuItem.setBackgroundResource(R.drawable.menu_button);
+                    menuItemIsSelected = false;
+                    timerBIsRunning = true;
+                    timerBIsPaused = false;
+                }
+                else if (timerBIsRunning){
+                    Log.i("11111111", "test");
+                    countDownTimerB.pause();
+                    timerBIsRunning = false;
+                    timerBIsPaused = true;
+                }
+                else if(timerBIsPaused){
+                    Log.i("111111112", "test");
+                    countDownTimerB.resume();
+                    timerBIsRunning = true;
+                    timerBIsPaused = false;
+                }
+
+            }
+        });
     }
+
+
 
     ///tell the OnTouchListener when to start moving the timerTextView and throw it away.
 
@@ -278,13 +329,13 @@ public class FryerTimer extends AppCompatActivity {
 //            case R.id.settiings_icon:
 //                Toast.makeText(this, "You have selected Bookmark Menu", Toast.LENGTH_SHORT).show();
 //                // Create a new intent to open the {@link PhrasesActivity}
-                int id = item.getItemId();
-                if (id == R.id.settiings_icon) {
-                    startActivity(new Intent(getApplicationContext(),SettingsActivity.class));
-                    return true;
-                }
-                return super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        if (id == R.id.settiings_icon) {
+            startActivity(new Intent(getApplicationContext(),SettingsActivity.class));
+            return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
 }
 
 
